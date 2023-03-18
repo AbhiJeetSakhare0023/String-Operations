e205eeff07783feeffe56d6cc88d6fcef8f15954
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-03-18 21:56:13 +0530

    reverse String

---
+++ src/Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+    public static void main(String[] args) {
+        String str="javadev";
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--)
+        {
+           rev=rev+str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
